ef86c0c170be6e5ccdb989bd0d23e8c9cb37d96c
ksh1024 <ksh251024@gmail.com>
2024-05-13 12:16:38 +0900

    views CharacterListView

--- 하프라이프/views.py
+++ 하프라이프/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from 하프라이프.models import Character
+
+
+class CharacterListView(ListView):
+    model = Character
+    # character_list = Character.objects.all() #DB에 모델 데이터 다 가져오자
+    #return render(request, '하프라이프/character_list.html', context={'character_list': character_list})  # #모델_list.html에 모델_list라는 키로 DB에 가져온 데이터 넣어서 render하자
+
+
