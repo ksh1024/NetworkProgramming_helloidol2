13bca2964d0e9e65181b70217006c24a8f2309cf
ksh1024 <ksh251024@gmail.com>
2024-05-23 15:53:22 +0900

    models/get_absolute_url(); views CharacterDetailView; templates character_detail.html, character_list.html; urls 하프라이프:character_detail pk

--- 하프라이프/views.py
+++ 하프라이프/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from 하프라이프.models import Character
 
 class CharacterListView(ListView):
-    model = Character #html파일로 모델이 들어감
+    model = Character #html파일로 모델이 들어감. Read
     # character_list = Character.objects.all() #DB에 모델 데이터 다 가져오자
     #return render(request, '하프라이프/character_list.html', context={'character_list': character_list})  # #모델_list.html에 모델_list라는 키로 DB에 가져온 데이터 넣어서 render하자
 
-
+class CharacterDetailView(DetailView):
+    model = Character
+    # character = Character.objects.get(pk=pk)
+    # return render(request, '하프라이프/character_detail.html' context={'character': character})
